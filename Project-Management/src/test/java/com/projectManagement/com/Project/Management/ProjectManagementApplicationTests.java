9121210499d6a9efc6e59f553224e250c671e284
Mahdy Muzzammil <156871399+HeyMahdy@users.noreply.github.com>
2024-10-29 22:22:54 +0600

    ohh another one

---
+++ Project-Management/src/test/java/com/projectManagement/com/Project/Management/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectManagement.com.Project.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
